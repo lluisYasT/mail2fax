aaea3bfab83dd651aaa62641840117e4c6adc1bc
Lluis <lluisyast@gmail.com>
2015-06-12 10:27:11 +0200

    DB authentication and callfile creation

--- mail2fax.py
+++ mail2fax.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
+from __future__ import print_function
 import mailbox
 import re
 import mysql.connector
+#import PythonMagick
 
 try:
     cnx = mysql.connector.connect(user='root', database='fax')
@@ -18,16 +20,37 @@ except mysql.connector.Error as err:
 def callerid_from_email(email_address):
     cursor = cnx.cursor()
     cursor.execute(
-        'SELECT number FROM fax_users WHERE email="%s"'%
+        'SELECT number FROM fax_users WHERE email="%s"' %
         email_address)
     number = cursor.fetchone()[0]
     cursor.close()
     return number
 
+def create_callfile(destination,callerid,email,filename):
+    callfile = open("/tmp/callfile", "w")
+    call = "Channel: Local/" + destination + "\@outbound-allroutes\n"
+    call += "CallerID: FAX <" + str(callerid) + ">\n"
+    call += "WaitTime: 60\n"
+    call += "Archive: yes\n"
+    call += "Context: outboundfax\n"
+    call += "Extension: s\n"
+    call += "Priority: 1\n"
+
+    # FAX Options
+    call += "Set: FAXFILE=" + filename + "\n"
+    call += "Set: FAXHEADER=" + str(callerid) + "\n"
+    call += "Set: LOCALID=" + str(callerid) + "\n"
+    call += "Set: EMAIL=" + email + "\n"
+    call += "Set: DESTINATION=" + destination + "\n"
+
+    print(call)
+
+    callfile.write(call)
+
+    callfile.close()
+
 
 if __name__ == "__main__":
-    print "Test:", callerid_from_email("lluisyast@gmail.com")
-
     try:
         root_mailbox = mailbox.Maildir('/home/lluis/Maildir', factory=None)
         for key in root_mailbox.iterkeys():
@@ -39,36 +62,40 @@ if __name__ == "__main__":
                 from_address = re.match("([^ ]+@[^ ]+\.\w+)", message['from'], flags=0)
                 if not from_address:
                     continue
-                print "\tTo:", to
-                print "\tFrom:", from_address.group(1)
+                print("\tTo:", to)
+                print("\tFrom:", from_address.group(1))
                 callerid = callerid_from_email(from_address.group(1))
-                print "CallerID:", callerid
+                print("CallerID:", callerid)
 
                 if not callerid:
-                    print "\nUser ", from_address.group(1), " not found\n"
-                    number = re.match("^(\d+)", to, flags=0)
-                    pdf_file = None
+                    print("\nUser ", from_address.group(1), " not found\n")
+                    continue
+                number = re.match("(\d+)", to, flags=0)
+                pdf_file = None
 
-                    if number :
-                        print "\tDir: ", message.get_subdir()
-                        #print "\tNumber: ", number.group(1)
-                        root_mailbox[key] = message
-                        root_mailbox.flush()
-                        print "\tFlags: ", root_mailbox[key].get_flags()
-                        if message.is_multipart():
-                            i = 0
-                            for part in message.walk():
-                                if part.get_content_type()=="application/pdf":
-                                    pdf_file_name = part.get_filename()
-                                    print "\tPart filename: ", pdf_file_name
-                                    print "\t\tContains a PDF file!"
-                                    pdf_file = part.get_payload(decode=True)
-                                    i += 1
+                if not number :
+                    continue
+                print("\tDir: ", message.get_subdir())
+                print("\tNumber: ", number.group(1))
+                root_mailbox[key] = message
+                root_mailbox.flush()
+                print("\tFlags: ", root_mailbox[key].get_flags())
+                if message.is_multipart():
+                    i = 0
+                    for part in message.walk():
+                        if part.get_content_type()=="application/pdf":
+                            pdf_file_name = part.get_filename()
+                            print("\tPart filename: ", pdf_file_name)
+                            print("\t\tContains a PDF file!")
+                            pdf_file = part.get_payload(decode=True)
+                            i += 1
 
-                                    if pdf_file:
-                                        f = open("/tmp/" + pdf_file_name, 'w')
-                                        f.write(pdf_file)
-                                        f.close()
+                            if pdf_file:
+                                f = open("/tmp/" + pdf_file_name, 'w')
+                                f.write(pdf_file)
+                                f.close()
+
+                                create_callfile(number.group(1), callerid, from_address.group(1), pdf_file_name)
     finally:
         root_mailbox.close()
 
