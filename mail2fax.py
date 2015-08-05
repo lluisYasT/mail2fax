900bb9cc99967816e27b6323fdcf57ad52266f24
Lluis <lluisyast@gmail.com>
2015-08-05 19:45:19 +0200

    It now accepts multiple pdf files. More verbose

--- mail2fax.py
+++ mail2fax.py
@@ -9,7 +9,7 @@ import subprocess
 
 #import PythonMagick
 
-MAILDIR = "/root/Maildir"
+MAILDIR = "/home/asterisk/Maildir"
 TMP_DIR="/tmp"
 
 
@@ -37,7 +37,18 @@ def callerid_from_email(email_address):
     cnx.close()
     return callerid
 
-def create_callfile(destination,callerid,email,filename):
+def create_callfile(destination,callerid,email,filenames):
+    if len(filenames) == 1:
+        filename = filenames[0]
+	fax_file_line = "Set: FAXOPT(filename)=" + filename + "\n"
+	fax_file_line += "Set: FAXFILE=" + filename + "\n"
+    elif len(filenames) > 1:
+        filename = ",".join(filenames)
+        filename_sendfax = "&".join(filenames)
+	fax_file_line = "Set: FAXOPT(filenames)=" + filename + "\n"
+	fax_file_line += "Set: FAXFILE=" + filename_sendfax + "\n"
+
+    print(filename)
     callfile_name = str(callerid) + str(destination) + str(time.time()) + ".call"
     callfile_path = TMP_DIR + "/" + callfile_name
     try:
@@ -56,7 +67,7 @@ def create_callfile(destination,callerid,email,filename):
     call += "Priority: 1\n"
 
     # FAX Options
-    call += "Set: FAXFILE=" + filename + "\n"
+    call += fax_file_line
     call += "Set: FAXHEADER=" + str(callerid) + "\n"
     call += "Set: LOCALID=" + str(callerid) + "\n"
     call += "Set: EMAIL=" + email + "\n"
@@ -72,6 +83,7 @@ if __name__ == "__main__":
     try:
         selected_mailbox = mailbox.Maildir(MAILDIR, factory=None)
         for key in selected_mailbox.iterkeys():
+            tiff_file_paths = []
             message = mailbox.MaildirMessage(selected_mailbox[key])
             if 'S' in message.get_flags():
                 continue
@@ -80,9 +92,10 @@ if __name__ == "__main__":
             selected_mailbox[key] = message
 
             to = message['to']
-            from_address = re.search("<?([a-zA-Z0-9_.]+@[a-zA-Z0-9_.]+\.\w{2,5})>?", message['from'], flags=0)
+            from_address = re.search("<?([a-zA-Z0-9_.]+@[a-zA-Z0-9_.-]+\.\w+)>?", message['from'], flags=0)
 
             if not from_address:
+		print(message['from'])
                 continue
 
             callerid = callerid_from_email(from_address.group(1))
@@ -90,15 +103,17 @@ if __name__ == "__main__":
             if callerid < 0:
                 print("\nUser ", from_address.group(1), " not found\n")
                 continue
-            number = re.search("^(\d+)@[a-zA-Z0-9_.]+\.\w{2,5}", to, flags=0)
+            number = re.search("<?(\d+)@[a-zA-Z0-9_.]+\.\w{2,5}>?", to, flags=0)
 
             if not number:
+                print("Wrong number in: ", to, "\n")
                 continue
 
             selected_mailbox[key] = message
             selected_mailbox.flush()
 
             if not message.is_multipart():
+                print("Not Multipart\n")
                 continue
             pdf_file = None
             for part in message.walk():
@@ -122,13 +137,15 @@ if __name__ == "__main__":
                         os.remove(pdf_file_path)
                     else:
                         print("Tiff conversion failed")
+                    
+                    tiff_file_paths.append(tiff_file_path)
 
-                    callfile = create_callfile(number.group(1), callerid, from_address.group(1), tiff_file_path)
-                    # We only want one PDF file
-                    break
-            if callfile:
-                print("FAX File created:", from_address.group(1), callerid, number.group(1))
-                os.rename(TMP_DIR + "/" + callfile, "/var/spool/asterisk/outgoing" + "/" + callfile)
+	    callfile = create_callfile(number.group(1), callerid, from_address.group(1), tiff_file_paths)
+	    # We only want one PDF file
+	    #break
+	    if callfile:
+		print("FAX File created:", from_address.group(1), callerid, number.group(1))
+		os.rename(TMP_DIR + "/" + callfile, "/var/spool/asterisk/outgoing" + "/" + callfile)
     finally:
         selected_mailbox.close()
 
